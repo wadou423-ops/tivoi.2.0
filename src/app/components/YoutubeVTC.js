82720a3039e706d2db7374c0dfccc5efa3c5a216
XMANUMO <venceslasetudiant@gmail.com>
2026-09-01 04:11:05 +0000

    Fix import useRef YoutubeVTC

--- src/app/components/YoutubeVTC.js
+++ src/app/components/YoutubeVTC.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { supabase } from "@/lib/supabaseClient";
 
 let apiPromise = null;
